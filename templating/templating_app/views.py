62e54264bc80dd26fb961973a331f87e480b9c5f
irvingad <irvingad@ncbi.nlm.nih.gov>
2016-03-10 15:53:58 -0500

    Created url, view, comment example

--- templating/templating_app/views.py
+++ templating/templating_app/views.py
@@ -9,3 +9,7 @@ def autoescape(request):
     autoescape_ex = "<p>This content has autoescape on.</p>"
     autoescape_off_ex = "<p>This content has autoescape off.</p>"
     return render(request, 'templating_app/autoescape.html', {"autoescape_ex": autoescape_ex, "autoescape_off_ex": autoescape_off_ex})
+
+
+def comment(request):
+    return render(request, 'templating_app/comment.html')
