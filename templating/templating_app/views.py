e7e3a2990714389862b89bdafb2b54803263c2fb
irvingad <irvingad@ncbi.nlm.nih.gov>
2016-03-21 17:47:33 -0400

    Escape ex

--- templating/templating_app/views.py
+++ templating/templating_app/views.py
@@ -120,4 +120,6 @@ def filters(request):
         {'name': 'Eddie', 'age': 36},
         {'name': 'Sebastian', 'age': 17},
     ]
-    return render(request, 'templating_app/filters.html', {"none_var": none_var, "people": people})
+    escape_ex = "<p>This HTML is escaped.</p>"
+    return render(request, 'templating_app/filters.html', {"none_var": none_var, "people": people,
+                                                           "escape_ex": escape_ex})
